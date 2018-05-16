a19597dd6e539fe7d71c936115d9cb0d863eb1a7
Tamunoenefaa Harry <tamunoenefaa@gmail.com>
2018-05-16 16:54:11 -0400

    Add age and rest the form each time

--- classnotes.js
+++ classnotes.js
@@ -3,10 +3,11 @@ const form = document.querySelector('form#userForm') //Selects form
 const handleSubmit = function(ev){ //Submit action
     ev.preventDefault()  
     const form = ev.target //<form id ="userForm"></> Basically saying where the action is used
-    const userName = form.userName.value // 
+    const userName = form.userName.value 
+    const age = form.age.value // 
     const users = document.querySelector('#users') //gets the line div below
-    users.innerHTML+= '<p>' + userName + '</p>' //each time the button is clicks adds name
-    form.userName.value = ''
+    users.innerHTML+= '<p>' + userName + ', ' + age + '</p>' //each time the button is clicks adds name
+    form.reset()
     
 }
 form.addEventListener('submit',handleSubmit) 
