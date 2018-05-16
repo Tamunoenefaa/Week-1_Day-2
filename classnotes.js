9f0fb61dd2d6adb99b0f2b03cb029b08371dc15e
Tamunoenefaa Harry <tamunoenefaa@gmail.com>
2018-05-16 16:00:11 -0400

    Add multiple name clearing the input each time

--- classnotes.js
+++ classnotes.js
@@ -4,8 +4,8 @@ const handleSubmit = function(ev){ //Submit action
     ev.preventDefault()  
     const form = ev.target //<form id ="userForm"></> Basically saying where the action is used
     const userName = form.userName.value // 
-    const users = document.querySelector('#users')
-    users.textContent+= ' ' + userName
+    const users = document.querySelector('#users') //gets the line div below
+    users.textContent+= ' ' + userName //each time the button is clicks adds name
     form.userName.value = ''
     
 }
